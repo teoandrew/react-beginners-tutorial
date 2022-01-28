e679380980aa92f8ff4cdeb2f466f66d33d172eb
Andrew Teo <andrwwteo@gmail.com>
2022-01-29 01:55:58 +0800

    Deleted index.js from src file too, coding from scratch now, according to tutorial

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import objects from React modules 
+/* 
+'react' is the module, React is the object we're importing from the module.
+We need to import React because Babel implicitly calls (references) React 
+i.e. React.createElement when it compiles JSX code. 
+*/
+import React from 'react'; 
+import ReactDOM from 'react-dom'; // to render elements from the virtual DOM into the real DOM
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const element = <h1>Hello World</h1>; // defining an element
+/* 
+in real world applications, you won't render elements just like that, you'll be rendering App
+components.
+*/
+console.log(element); // logging element onto the console
+ReactDOM.render(element, document.getElementById('root')); 
+// ReactDOM gets a reference of 'element' and renders it inside the element referenced in the second parameter
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
